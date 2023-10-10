2a154904e7ed221090f57b8c12fb9cd7ab7a9b06
Sergio Ramos Suárez <sergioramos@ieselrincon.es>
2023-10-10 13:41:30 +0100

    Añadimos código de ejemplo para seleccionar un valor por defecto en un desplegable :

---
+++ UT2 - Manejo de formularios y ficheros/a2.2/seleccionar_opcion_defecto.php
@@ -0,0 +1,33 @@
+<?php
+
+// Valor que deberá estar seleccionado
+$color_seleccionado = "verde";
+
+// Inicializamos las variables a ""
+$selected_rojo = $selected_verde = $selected_azul = "";
+
+// En función del valor por defecto, seleccionamos una u otra opción
+switch ($color_seleccionado) {
+    case "rojo":
+        $selected_rojo = "selected";
+        break;
+    case "verde":
+        $selected_verde = "selected";
+        break;
+    case "azul":
+        $selected_azul = "selected";
+        break;
+}
+
+
+?>
+
+<html>
+
+    <select name="color" multiple>
+        <option value="rojo"    <?= $selected_rojo ?>   >Rojo</option>
+        <option value="verde"   <?= $selected_verde ?>  >Verde</option>
+        <option value="azul"    <?php echo $selected_azul ?>   >Azul</option>
+    </select>
+
+</html>
