fa6f7c8ddb372b28214a27c988f6f662b575c7b7
Sergio <sergioramos@ieselrincon.es>
2023-10-19 12:22:29 +0100

    Ejemplo file_upload

--- UT2 - Manejo de formularios y ficheros/file_upload/recibe_formulario.php
+++ UT2 - Manejo de formularios y ficheros/file_upload/recibe_formulario.php
@@ -2,10 +2,15 @@
     $directorio = "./ficheros/";
     $fichero = $_FILES["fichero"]["name"]
 
+    print_r($_FILES);
+
+    /*
+
     if (move_uploaded_file($_FILES["fichero"]["tmp_name"], $directorio)) {
         echo "El fichero se ha subido correctamente";
     } else {
         echo "Error al subir el fichero al servidor";
     }
+    */
 }
 ?>
